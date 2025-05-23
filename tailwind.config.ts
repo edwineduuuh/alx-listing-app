564604c754c2a23c75809ce5f1160607ef9e358c
edwineduuuh <edumwas92@gmail.com>
2025-05-23 16:53:31 +0300

    Initial commit: Set up ALX Listing App with Next.js, TypeScript, Tailwind

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+// module.exports = {
+//   content: [
+//     './pages/**/*.{ts,tsx}',
+//     './components/**/*.{js,ts,jsx,tsx}',
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+module.exports = {
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
